87998fa3f3d2304002e8d642a75a02e9df993cc7
Natalia_Sherchenkova <tataterer@gmail.com>
2021-02-28 23:21:52 +0200

    Refactor code using methods

--- directory.rb
+++ directory.rb
@@ -1,64 +1,64 @@
-def print_header(students)
-  if !students.empty?
-    puts "The students of Villains Academy"
-    puts "-------------"
-  end
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def print_footer(students)
-  if !students.empty?
-    puts "Overall, we have #{students.count} great students"
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
   name = gets.chomp
-
-  # while the name is not empty, repeat this code
   while !name.empty? do
-  # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-  # get another name from the user
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
   name = gets.chomp
   end
-  # return the array of students
-  students
+end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
 
-def interactive_menu
-  students = []
-  loop do
-  # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
-  # 2. read the input and save it into a variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header(students)
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
 end
+
+def print_header
+  if !@students.empty?
+    puts "The students of Villains Academy"
+    puts "-------------"
+  end
+end
+
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer
+  if !@students.empty?
+    puts "Overall, we have #{@students.count} great students"
+  end
+end
+
 
 interactive_menu
